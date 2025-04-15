48287ac57b11b9e71297f354f2eaeb064f599f1c
Denis Onsare <denisonsare@gmail.com>
2025-04-15 21:14:35 +0300

    Responsive design

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -8,10 +8,10 @@ export default function CallToAction() {
     <section className='w-full mx-auto my-20   2xl:max-w-[1250px] lg:max-w-[1250px]  bg-[linear-gradient(90deg,_#004359_0%,_#063240_100%)] rounded-lg'>
       <div className='mx-auto flex max-w-7xl items-center justify-between py-6 lg:py-14 lg:px-8'>
         <div className='w-full max-w-[453px] mx-auto text-center flex flex-col items-center justify-center space-y-4'>
-          <h1 className='text-[rgba(255,255,255,1)] font-bold text-[44px] leading-[48px] tracking-[-1%]'>
+          <h1 className='text-[rgba(255,255,255,1)] font-bold text-[30px] lg:text-[44px] leading-[48px] tracking-[-1%]'>
             Works Seamlessly with Your Consulting Stack.
           </h1>
-          <p className=' text-[rgba(255,255,255,0.64)] font-normal text-[17px] leading-[27px] '>
+          <p className=' text-[rgba(255,255,255,0.64)] font-normal text-[17px] lg:leading-[27px] '>
             Integrate with the tools you already use to enhance your consulting
             workflow.
           </p>
